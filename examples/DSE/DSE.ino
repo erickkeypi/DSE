f21ea69fec2d8e66f57598d355b23fb9e923f373
erickkeypi <erickrgm24@gmail.com>
2020-08-16 10:27:12 -0400

    cambiado ejemplo

--- examples/DSE/DSE.ino
+++ examples/DSE/DSE.ino
@@ -1,9 +1,20 @@
-#include "DSE.h"
+#include <DSE.h>
 
 byte mac[] = {0x60, 0x52, 0xD0, 0x06, 0x68, 0x98};//P1AM-ETH MAC
 IPAddress ip(192, 168, 137, 177);//P1AM-ETH IP
 
-DSE modulo = DSE(DSE_8660MKII, IPAddress(192, 168, 137,  126), "Master 1");
+
+DSE modulos[9] = {
+  DSE(DSE_8660MKII, IPAddress(192, 168, 137,  126), "Master 1"),
+  DSE(DSE_8660MKII, IPAddress(192, 168, 137,  126), "Master 2"),
+  DSE(DSE_8660MKII, IPAddress(192, 168, 137,  126), "Master 3"),
+  DSE(DSE_8660MKII, IPAddress(192, 168, 137,  126), "Master 4"),
+  DSE(DSE_8660MKII, IPAddress(192, 168, 137,  126), "Master 5"),
+  DSE(DSE_8660MKII, IPAddress(192, 168, 137,  126), "Master 6"),
+  DSE(DSE_8660MKII, IPAddress(192, 168, 137,  126), "Master 7"),
+  DSE(DSE_8660MKII, IPAddress(192, 168, 137,  126), "Master 8"),
+  DSE(DSE_8660MKII, IPAddress(192, 168, 137,  126), "Master 9")
+};
 
 void setup(){
   Serial.begin(115200);
@@ -11,19 +22,24 @@ void setup(){
   Serial.println("#####INIT#####");
 
   Ethernet.begin(mac,ip);
-  modulo.begin();
+  for(int i=0;i<9;i++){
+    modulos[i].begin();
+  }
 }
 
 void loop(){
-  if(modulo.connect()){
-    modulo.update();
-    Serial.println(millis()/1000);
-    Serial.print("Frecuencia: ");
-    Serial.println(modulo.HZ);
-    modulo.stop();
+  for(int i=0;i<9;i++){
+    if(modulos[i].connect()){
+      modulos[i].update();
+      Serial.print(modulos[i].getName());
+      Serial.print(" frecuencia: ");
+      Serial.println(modulos[i].HZ);
+      modulos[i].stop();
+    }
+    else{
+      Serial.println("Error trying to connect");
+    }
   }
-  else{
-    Serial.println("Error trying to connect");
-  }
+  Serial.println(millis()/1000);
   delay(1000);
 }
